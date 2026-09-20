6f33ded3b83be00c0b9518ccd4343ec7878fe283
DeVSakharov <sakhirsp@gmail.com>
2026-09-21 02:11:16 +0300

    Migrate application to Spring Boot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,48 +1,12 @@
 package com.example;
 
-import com.example.config.AppConfig;
-import jakarta.servlet.MultipartConfigElement;
-import org.apache.catalina.Context;
-import org.apache.catalina.Wrapper;
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
-    public static void main(String[] args) throws Exception {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        tomcat.getConnector();
 
-        Context context = tomcat.addContext("", null);
-
-        AnnotationConfigWebApplicationContext springContext =
-                new AnnotationConfigWebApplicationContext();
-
-        springContext.register(AppConfig.class);
-        springContext.setServletContext(context.getServletContext());
-        springContext.refresh();
-
-        DispatcherServlet dispatcherServlet =
-                new DispatcherServlet(springContext);
-
-        Wrapper wrapper = Tomcat.addServlet(
-                context,
-                "dispatcher",
-                dispatcherServlet
-        );
-
-        wrapper.setLoadOnStartup(1);
-        context.addServletMappingDecoded("/", "dispatcher");
-
-        wrapper.setMultipartConfigElement(new MultipartConfigElement(
-                System.getProperty("java.io.tmpdir"),
-                10_000_000,
-                20_000_000,
-                1_000_000
-        ));
-
-        tomcat.start();
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,27 +1,13 @@
 package com.example.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.support.TransactionTemplate;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-
 @Configuration
-@EnableWebMvc
-@EnableTransactionManagement
-@ComponentScan("com.example")
 public class AppConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -34,39 +20,5 @@ public class AppConfig {
                         .allowCredentials(true);
             }
         };
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/blog");
-        config.setUsername("blog_user");
-        config.setPassword("blog_password");
-        config.setDriverClassName("org.postgresql.Driver");
-
-        return new HikariDataSource(config);
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    @Bean
-    public TransactionTemplate transactionTemplate(
-            PlatformTransactionManager transactionManager
-    ) {
-        return new TransactionTemplate(transactionManager);
-    }
-
-    @Bean
-    public StandardServletMultipartResolver multipartResolver() {
-        return new StandardServletMultipartResolver();
     }
 }
